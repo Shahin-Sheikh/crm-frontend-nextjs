401923ee54c38f2b05b4b470d68f8d1fa9b246a4
Md. Shahin Sheikh <59409715+Shahin-Sheikh@users.noreply.github.com>
2023-11-19 22:55:27 +0600

    Updated

--- src/app/features/auth/signin/signin-form.component.tsx
+++ src/app/features/auth/signin/signin-form.component.tsx
@@ -2,14 +2,9 @@ import Image from "next/image";
 function SignIn() {
   return (
     <div className="flex flex-row bg-whtie-500 rounded ">
-      <div className="bg-blue-500 rounded h-[870px] w-[940px]">
+      <div className="bg-[#3F8CFF] rounded h-[870px] w-[940px]">
         <div className="flex flex-row gap-4 pt-[60px] pl-[94px]">
-          <img
-            src="https://www.contexteditor.org/wp-content/uploads/2021/11/logo.gif"
-            alt="Company Logo"
-            width="60px"
-            height="50px"
-          />
+          <img src="/logo.ico" alt="Company Logo" width="80px" height="50px" />
           <h1 className="p-3 font-[700] text-[30px] font-sans">Woorkroom</h1>
         </div>
         <div className="pt-[60px] pl-[94px]">
@@ -18,11 +13,7 @@ function SignIn() {
           </h1>
         </div>
         <div className="pt-[60px] pl-[94px]">
-          <img
-            src="https://wp-asset.groww.in/wp-content/uploads/2019/08/26114746/How-are-Different-Stocks-Categorized.jpg "
-            width={600}
-            height={600}
-          />
+          <img src="/background.png " width={600} height={600} />
         </div>
       </div>
       <div className="bg-white p-20 rounded h-[870px] w-[940px]">sss</div>
